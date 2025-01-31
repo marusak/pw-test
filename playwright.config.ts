bea2d568a1d035397bbad1c37e9604f202435f66
Matej Marusak <marusak.matej@gmail.com>
2025-01-31 18:59:09 +0100

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'], ['playwright-ctrf-json-reporter', {}], ['dot']],
+  reporter: [['html'], ['playwright-ctrf-json-reporter', {}], ['./ci-reporter']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,26 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('Flaky', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  let txt = "Fast and reliable end-to-end testing for modern web apps | Playwright";
+  if (Math.floor(Math.random() * 10) < 5)
+      txt = "fdflsf";
+  await expect(page).toHaveTitle(txt);
 });
 
 test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+test.skip('skipitty skip skip', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
